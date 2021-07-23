77cb72c2e7e6854b3825677e81d429ccceb6f2a8
werlleymendes <werlleymendes@hotmail.com>
2021-07-23 16:17:42 -0300

    Consulta de Fornecedores do VRSOFTWARE

---
+++ sql/consultafornVRSOFT.sql
@@ -0,0 +1,60 @@
+SELECT * FROM fornecedor limit 10;
+
+SELECT f.id, f.razaosocial, f.nomefantasia, f.endereco, f.numero, f.bairro, m.descricao AS municipio, f.cep, e.descricao AS estado, 
+f.telefone, ti.descricao as "tipo inscricao", f.inscricaoestadual, f.cnpj as "cnpj/cpf", tr.descricao as "tipo pagamento", 
+fpp.diasentregapedido AS "dias entrega pedido", fpp.diasatualizapedidoparcial as "dias atual. ped parcial", MAX(ne.dataentrada)
+FROM fornecedor as f JOIN municipio as m ON f.id_municipio = m.id
+JOIN estado AS e ON f.id_estado = e.id
+FULL JOIN tipoinscricao AS ti ON f.id_tipoinscricao = ti.id
+FULL JOIN tiporecebimento AS tr ON f.id_tiporecebimento = tr.id
+FULL JOIN fornecedorprazopedido AS fpp ON f.id = fpp.id_fornecedor
+JOIN notaentrada as ne ON f.id = ne.id_fornecedor
+WHERE f.id_situacaocadastro = 1
+AND fpp.id_loja = 1
+AND ne.dataentrada > '2020-07-15'
+GROUP BY f.id, m.descricao, e.descricao, ti.descricao, tr.descricao, fpp.diasentregapedido, fpp.diasatualizapedidoparcial
+ORDER BY f.id
+;
+
+SELECT *
+FROM fornecedorprazo AS fp 
+WHERE fp.id_loja = 1
+AND fp.id_divisaofornecedor = 0
+ORDER BY fp.id_fornecedor;
+
+
+SELECT * FROM tipopagamento;
+
+SELECT * FROM tiporecebimento;
+
+SELECT * FROM tipofornecedor;
+
+SELECT tablename, schemaname FROM pg_tables;
+
+
+COPY
+(
+    SELECT tablename, schemaname FROM pg_tables
+)
+TO 'C:/git/sql/fornecedor.csv'
+DELIMITER ';'
+CSV HEADER
+
+
+SELECT * FROM agendafornecedor order by id_fornecedor;
+
+SELECT * FROM fornecedorconfiguracaodatacorte order by id_fornecedor;
+
+SELECT * FROM fornecedorpagamento order by id_fornecedor;
+
+SELECT * FROM fornecedorprazopedido where id_loja = 1 order by id_fornecedor;
+
+SELECT * FROM tipoagendafornecedor;
+
+SELECT * FROM fornecedorprazo where id_loja = 1 order by id_fornecedor, id_divisaofornecedor;
+
+SELECT * FROM fornecedordocumento;
+
+SELECT * FROM notaentrada limit 10;
+
+SELECT id_fornecedor, max(dataentrada) FROM notaentrada GROUP by id_fornecedor ORDER BY 1;
